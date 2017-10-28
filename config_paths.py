53a11194800c328b65be43f01474a4750cb14a1a
Nojan <nojan.pasha@gmail.com>
2017-10-28 00:58:22 -0700

    Yelp recommender system

---
+++ config_paths.py
@@ -0,0 +1,6 @@
+USERS_PATH='/home/nojan/Nojan/GitHub_repos/Restaurant-recommender-system-for-Yelp/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_user.json'
+BUSINESS_PATH='/home/nojan/Nojan/GitHub_repos/Restaurant-recommender-system-for-Yelp/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json'
+CHECKIN_PATH='/home/nojan/Nojan/GitHub_repos/Restaurant-recommender-system-for-Yelp//yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_checkin.json'
+REVIEW_PATH='/home/nojan/Nojan/GitHub_repos/Restaurant-recommender-system-for-Yelp/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
+TIP_PATH='/home/nojan/Nojan/GitHub_repos/Restaurant-recommender-system-for-Yelp/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_tip.json'
+
